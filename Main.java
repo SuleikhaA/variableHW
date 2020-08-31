525a036bed2d755199eed75f6a096a20f99f2390
SuleikhaA <31260562+SuleikhaA@users.noreply.github.com>
2020-08-31 20:39:55 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x;
+        float y;
+        String z;
+        x = 50;
+        y = (float)24.99;
+        z = "Bobby Johnson";
+            System.out.println( " Test score initialized is: " + x);
+        System.out.println(" Price tag: " + y);
+        System.out.println( " Username: " + z);
+
+    }
+}
